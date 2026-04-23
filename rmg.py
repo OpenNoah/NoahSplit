91c3ddcfc10c27bfd0fe9abbd2ce6adc7e17a505
Norman Zhi <normanzyb@gmail.com>
2026-04-23 23:36:34 +0100

    Fix NP980 directory parsing

--- rmg.py
+++ rmg.py
@@ -11,17 +11,10 @@ def bin_to_str(ba):
 
 def save(save_path, metadata, data):
     if metadata["type"] == "dir":
-        if metadata["ext"]:
-            raise RuntimeError(f"Extension is not empty for a directory path: {metadata}")
-        if metadata["size"] != 0:
-            raise RuntimeError(f"Size is not 0 for a directory path: {metadata}")
-        # TODO: How to decode the directory path?
-        d_path = os.path.join(save_path, metadata['dir'], metadata['name'])
-        os.makedirs(d_path, exist_ok=True)
+        os.makedirs(os.path.join(save_path, metadata['path'], metadata['name']), exist_ok=True)
     else:
-        # TODO: How to decode the directory path?
         ext = '.' + metadata['ext'] if metadata['ext'] else ''
-        f_path = os.path.join(save_path, metadata['dir'], f"{metadata['name']}{ext}")
+        f_path = os.path.join(save_path, metadata['path'], f"{metadata['name']}{ext}")
         print(f_path)
         os.makedirs(os.path.dirname(f_path), exist_ok=True)
         with open(f_path, "wb") as f_out:
@@ -40,17 +33,34 @@ def main():
     with open(args.pkg_file, "rb") as f_in:
         header = f_in.read(32)
         pkg_data["header"] = header
-        while entry := f_in.read(32):
-            if entry[0] == 0:
-                break
-            f_meta = {}
-            f_meta["name"] = bin_to_str(entry[0:8])[0].strip()
-            f_meta["ext"] = bin_to_str(entry[8:11])[0].strip()
-            f_meta["type"] = {0x11: "dir", 0x21: "file"}[entry[11]]
-            f_meta["offset"] = int.from_bytes(entry[20:24], 'little')
-            f_meta["size"] = int.from_bytes(entry[24:28], 'little')
-            f_meta["dir"] = entry[28:32].hex()
-            pkg_data["files"].append(f_meta)
+
+        def parse_dir(path, count):
+            dir_list = []
+            for _ in range(count):
+                entry = f_in.read(32)
+                f_meta = {}
+                f_meta["path"] = path
+                f_meta["name"] = bin_to_str(entry[0:8])[0].strip()
+                f_meta["type"] = {0x11: "dir", 0x21: "file"}[entry[11]]
+                if f_meta["type"] == "dir":
+                    f_ext = bin_to_str(entry[8:11])[0].strip()
+                    if f_ext:
+                        raise RuntimeError(f"Extension is not empty for a directory path: {entry}")
+                    f_size = int.from_bytes(entry[24:28], 'little')
+                    if f_size != 0:
+                        raise RuntimeError(f"Size is not 0 for a directory path: {entry}")
+                    f_meta["entries"] = int.from_bytes(entry[22:24], 'little')
+                    dir_list.append([os.path.join(path, f_meta["name"]), f_meta["entries"]])
+                else:
+                    f_meta["ext"] = bin_to_str(entry[8:11])[0].strip()
+                    f_meta["offset"] = int.from_bytes(entry[20:24], 'little')
+                    f_meta["size"] = int.from_bytes(entry[24:28], 'little')
+                pkg_data["files"].append(f_meta)
+
+            for d_path, d_count in dir_list:
+                parse_dir(d_path, d_count)
+
+        parse_dir("", 1)
 
         f_in.seek(0, os.SEEK_SET)
         f_data = f_in.read()
